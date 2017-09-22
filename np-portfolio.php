b6d5343c71c083a06159c7257008b274af88c0d2
Your Name <you@example.com>
2017-09-22 19:31:54 +0300

    First commit

---
+++ np-portfolio.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Portfolio Plugin by Nataliya
+Plugin URI: https://np-portfolio.com/
+Description: Portfolio Plugin for Courses WP
+Version: 1.0
+Author: Nataliya
+Author URI: https://automattic.com/wordpress-plugins/
+License: GPLv2 or later
+Text Domain: np-portfolio
+*/
