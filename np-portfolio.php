2167db0f0c250ef3e6d9ede605832a4ece09818c
Your Name <you@example.com>
2017-10-06 00:49:58 +0300

    added columns in plugin portfolio

--- np-portfolio.php
+++ np-portfolio.php
@@ -25,3 +25,28 @@ function np_admin_scripts() {
   wp_enqueue_script('np-admin', plugin_dir_url(__FILE__) . 'js/admin.js');
   wp_enqueue_style('np-datepicker-style', '//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css');
 }
+
+// add columns
+add_filter( 'manage_posts_columns' , 'np_posts_columns2' );
+
+function np_posts_columns2( $columns ) {
+    $columns['date_column'] = __( 'Date column', 'np-portfolio' );
+    $columns['link_column'] = __( 'Link column', 'np-portfolio' );
+
+    return $columns;
+}
+
+add_action( 'manage_posts_custom_column' , 'np_posts_columns_content2', 10, 2 );
+
+function np_posts_columns_content2( $column, $post_id ) {
+    if ($column == 'date_column') {
+        // echo get_the_date( $d = '', $post_id ); // выводит дату публикации самого поста
+
+        echo get_post_meta(get_the_ID(), '_np_portfolio_date', true); // выводит дату, указанную в кастомном поле
+    }
+    if ($column == 'link_column') {
+        // echo get_the_date( $d = '', $post_id ); // выводит дату публикации самого поста
+
+        echo '<a href="http://'. get_post_meta(get_the_ID(), '_np_portfolio_link', true) . '" target="_blank">'. get_post_meta(get_the_ID(), '_np_portfolio_link', true) .'</a>'  ;; // выводит дату, указанную в кастомном поле
+    }
+}
